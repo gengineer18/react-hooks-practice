889325dd1d14b1a99192146231cab2a600b4cf3d
妹尾弦 <gengineer.web@gmail.com>
2020-05-30 23:58:08 +0900

    配列の学習

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 // import Counter from './component/Counter'
 // import CounterHook from './component/CounterHook'
-import FormHook from './component/FormHook'
+// import FormHook from './component/FormHook'
+import { ItemHook } from './component/ItemHook'
 
 function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
       {/* <CounterHook /> */}
-      <FormHook />
+      {/* <FormHook /> */}
+      <ItemHook />
     </div>
   );
 }
