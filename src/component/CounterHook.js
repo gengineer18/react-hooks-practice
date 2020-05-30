d9d860baf31d88507329b383fdfb30ddc5b2b88b
妹尾弦 <gengineer.web@gmail.com>
2020-05-30 21:21:07 +0900

    UseStateで変数を扱う

---
+++ src/component/CounterHook.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+export default function CounterHook() {
+  const [count, setCount] = useState(0)
+
+  const incrementCount = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={incrementCount}>CountUp</button>
+    </div>
+  )
+}
