d8c0b8ada021095d34c14489b99e3e1a2f3d3886
妹尾弦 <gengineer.web@gmail.com>
2020-05-30 21:52:14 +0900

    ループの処理について

--- src/component/CounterHook.js
+++ src/component/CounterHook.js
@@ -4,13 +4,19 @@ export default function CounterHook() {
   const [count, setCount] = useState(0)
 
   const incrementCount = () => {
-    setCount(count + 1)
+    setCount(prevCount => prevCount + 1)
+  }
+  const incrementCountTen = () => {
+    for (let i = 0; i < 10; i++) {
+      setCount(prevCount => prevCount + 1)
+    }
   }
 
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={incrementCount}>CountUp</button>
+      <button onClick={incrementCountTen}>CountUp +10</button>
     </div>
   )
 }
