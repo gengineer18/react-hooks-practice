792475643f758480c3f348245bc56e04dd6531df
妹尾弦 <gengineer.web@gmail.com>
2020-05-30 22:11:31 +0900

    オブジェクトの学習

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import Counter from './component/Counter'
-import CounterHook from './component/CounterHook'
+// import Counter from './component/Counter'
+// import CounterHook from './component/CounterHook'
+import FormHook from './component/FormHook'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <CounterHook />
+      {/* <Counter /> */}
+      {/* <CounterHook /> */}
+      <FormHook />
     </div>
   );
 }
