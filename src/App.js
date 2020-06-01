b0f323aeb51e25ac667c657f4ab8ffdc1c0c49b5
妹尾弦 <gengineer.web@gmail.com>
2020-06-01 21:44:29 +0900

    useReducerとuseContextの組み合わせ

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { createContext, useReducer } from 'react';
 import './App.css';
-import { CounterReducer } from './component/CounterReducer';
+import { ComponentA } from './component/ComponentA';
+import { ComponentB } from './component/ComponentB';
+import { ComponentC } from './component/ComponentC';
 
+export const CountContext = createContext()
+
+const initialState = {
+  firstCounter: 0
+}
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'increment1':
+      return { ...state, firstCounter: state.firstCounter + action.value }
+    case 'decrement1':
+      return { ...state, firstCounter: state.firstCounter - action.value }
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="App">
-      <CounterReducer />
+      <h1>カウント：{count.firstCounter}</h1>
+      <CountContext.Provider
+        value={{countState: count, countDispatch: dispatch}}
+      >
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </CountContext.Provider>
     </div>
   );
 }
